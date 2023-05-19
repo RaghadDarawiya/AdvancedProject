93829cb2646fb45a69be1e37a818b417d7a6e606
RaghadDarawiya <100946129+RaghadDarawiya@users.noreply.github.com>
2023-05-19 16:45:21 +0300

    Update and rename job_listings .js to joblistings .js
    ubdate joblistings model

---
+++ models/joblistings .js
@@ -0,0 +1,41 @@
+const mysql = require('mysql2');
+
+// Create a MySQL connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '123raghad',
+  database: 'linkedin',
+});
+
+class JobListing {
+  constructor(jobid , id, jtitle, jdescription, jrequirements, SalaryRange, dateposted) {
+    this.jobid = jobid;
+    this.id = id;
+    this.jtitle = jtitle;
+    this.jdescription = jdescription;
+    this.jrequirements = jrequirements;
+    this.SalaryRange = SalaryRange;
+    this.dateposted = dateposted;
+  }
+
+  save(callback) {
+    // Insert the job listing into the database
+    db.query(
+      'INSERT INTO job_listings (jobid,id,jobtitle, jobdes, jobreq, SalaryRange, dateposted) VALUES (?,?,?, ?, ?, ?, ?)',
+      [this.jobid , this.id ,this.jtitle, this.jdescription, this.jrequirements, this.SalaryRange, this.dateposted],
+      (err, result) => {
+        if (err) {
+          console.error('Error creating job listing: ', err);
+          callback(err, null);
+        } else {
+          callback(null, result);
+        }
+      }
+    );
+  }
+
+  // You can also define additional methods or validation logic here if needed
+}
+
+module.exports = JobListing;
